43360f0714c65361661ee7e73b84b7c68693614b
charlotteForever <fernweh1001@163.com>
2022-01-05 23:21:17 +0800

    update

---
+++ 14.单文件组件/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <school></school>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import Student from "./Student.vue";
+export default {
+  components: { Schoool },
+};
+</script>
+
+<style>
+</style>
